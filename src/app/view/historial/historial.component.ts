0db45d67a49c7b3b3b2cd354fe30c2c9f8e6e071
Cesar-CAAR <cesar.andres5242@gmail.com>
2025-03-21 09:05:08 -0700

    actualizacion de ruta

--- src/app/view/historial/historial.component.ts
+++ src/app/view/historial/historial.component.ts
@@ -8,28 +8,21 @@ import { Router } from '@angular/router';
   styleUrl: './historial.component.css'
 })
 export class HistorialComponent {
-
-}
-
-@Component({
-  selector: 'app-alarma',
-  imports: [],
-  templateUrl: './alarma.component.html',
-  styleUrl: './alarma.component.css'
-})
-
-export class AlarmaComponent {
   constructor(private router: Router) {}
 
   onInicio() {
     this.router.navigate(['/inicio']);
   }
 
-  onRegistros() {
-    this.router.navigate(['/registros']);
+  onHistorial() {
+    this.router.navigate(['/historial']);
   }
 
-  onRecuperar() {
-    this.router.navigate(['/recuperar']);
+  onAlarma() {
+    this.router.navigate(['/alarma']);
+  }
+
+  onLogin() {
+    this.router.navigate(['/login']);
   }
 }
